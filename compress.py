f7b0237e522d4da49d545cd740944fcbba686eff
Amirreza Tarabkhah <amirreza.tarabkhah@gmail.com>
2022-06-28 22:58:07 +0430

    Complete quantized cosine blocking

--- compress.py
+++ compress.py
@@ -3,6 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack
 
+# standard_luminance_quantization_table
+QY = np.array([
+    [16,11,10,16,24,40,51,61],
+    [12,12,14,19,26,48,60,55],
+    [14,13,16,24,40,57,69,56],
+    [14,17,22,29,51,87,80,62],
+    [18,22,37,56,68,109,103,77],
+    [24,35,55,64,81,104,113,92],
+    [49,64,78,87,103,121,120,101],
+    [72,92,95,98,112,100,103,99]]
+)
+
+# standard_chrominance_quantization_table
+QC = np.array([
+    [17,18,24,47,99,99,99,99],
+    [18,21,26,66,99,99,99,99],
+    [24,26,56,99,99,99,99,99],
+    [47,66,99,99,99,99,99,99],
+    [99,99,99,99,99,99,99,99],
+    [99,99,99,99,99,99,99,99],
+    [99,99,99,99,99,99,99,99],
+    [99,99,99,99,99,99,99,99]]
+)
+
 # function convert rgb to ycbcr
 def rgb2ycbcr(img):
     ycbcr = np.zeros(img.shape)
@@ -29,80 +53,39 @@ def chroma_subsampling_420(img):
     # img = rgb2ycbcr(img)
     print(img.shape)
     show_image(img, 'YCbCr Chroma Image')
-    return cosine_blocking(img)
+    return img
 
-# function cosine blocking 8x8 after chroma subsampling
+# function cosine blocking 8x8
 def cosine_blocking(img):
-    # convert rgb to ycbcr
-    ycbcr = rgb2ycbcr(img)
-    # convert ycbcr to y
-    y = ycbcr[:,:,0]
-    # convert ycbcr to cb and cr
-    cb = ycbcr[:,:,1] - 128
-    cr = ycbcr[:,:,2] - 128
-    # calculate fft of y
-    ffty = scipy.fftpack.fft2(y)
-    # calculate fft of cb and cr
-    fftcb = scipy.fftpack.fft2(cb)
-    fftcr = scipy.fftpack.fft2(cr)
-    # calculate fft of ycbcr
-    fftycbcr = np.zeros(ycbcr.shape)
-    fftycbcr[:,:,0] = ffty
-    fftycbcr[:,:,1] = fftcb
-    fftycbcr[:,:,2] = fftcr
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.fft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = fftycbcr / 8
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate
+    img = chroma_subsampling_420(img)
+    print(img[img.shape[0]//2, img.shape[1]//2, :])
+    width = img.shape[0]
+    height = img.shape[1]
+    dct = []
+    # img.save("imgage_after_subsampling.jpg")
+    for i in range(0, width, 8) :
+        for j in range(0, height, 8) : 
+            block = [[0] * 8] * 8
+            for block_x_axis in range(i, i + 8) : 
+                for block_y_axis in range(j, j + 8) : 
+                    if (block_x_axis >= width) or (block_y_axis >= height) : 
+                        data = [0, 0, 0]
+                    else : 
+                        data = img[block_x_axis, block_y_axis, :]
+                    block[block_x_axis - i][block_y_axis - j] = data
+            dct.append(scipy.fftpack.dct(scipy.fftpack.dct(block, axis = 0, norm = 'ortho'), axis = 1, norm = 'ortho'))
+    return dct
 
-# function compress jpeg
-def compress(img, quality):
-    # convert rgb to ycbcr
-    ycbcr = rgb2ycbcr(img)
-    # convert ycbcr to y
-    y = ycbcr[:,:,0]
-    # convert ycbcr to cb and cr
-    cb = ycbcr[:,:,1] - 128
-    cr = ycbcr[:,:,2] - 128
-    # calculate fft of y
-    ffty = scipy.fftpack.fft2(y)
-    # calculate fft of cb and cr
-    fftcb = scipy.fftpack.fft2(cb)
-    fftcr = scipy.fftpack.fft2(cr)
-    # calculate fft of ycbcr
-    fftycbcr = np.zeros(ycbcr.shape)
-    fftycbcr[:,:,0] = ffty
-    fftycbcr[:,:,1] = fftcb
-    fftycbcr[:,:,2] = fftcr
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.fft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = fftycbcr / quality
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
-    # calculate fft of ycbcr
-    fftycbcr = scipy.fftpack.ifft2(fftycbcr)
+# function to quantize
+def quantize(img):
+    img = cosine_blocking(img)
+    img_quantized = img
+    for i in range(len(img)):
+        img_quantized[i][:, :, 0] = img[i][:, :, 0] // QY
+        img_quantized[i][:, :, 1] = img[i][:, :, 1] // QC
+        img_quantized[i][:, :, 2] = img[i][:, :, 2] // QC
+    print(img_quantized[106])
+    return img_quantized
 
 # function open jpeg image
 def open_jpeg(filename):
@@ -124,7 +107,7 @@ if __name__ == '__main__':
     show_image(img, "Original image")
     # plt.show()
 
-    chroma_subsampling_420(img)
+    quantize(img)
 
     plt.show()
 
