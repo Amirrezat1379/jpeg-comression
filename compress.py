8d2780d4a8f0023dae05dfadbd91aa78c610fa14
Amirreza Tarabkhah <amirreza.tarabkhah@gmail.com>
2022-06-30 00:13:09 +0430

    Complete project :D

--- compress.py
+++ compress.py
@@ -1,8 +1,10 @@
+from operator import le
 from PIL import Image
 import numpy as np 
 import matplotlib.pyplot as plt
-import scipy.fftpack
-
+import cv2
+import huffman
+import pickle
 # standard_luminance_quantization_table
 QY = np.array([
     [16,11,10,16,24,40,51,61],
@@ -58,7 +60,8 @@ def chroma_subsampling_420(img):
 # function cosine blocking 8x8
 def cosine_blocking(img):
     img = chroma_subsampling_420(img)
-    print(img[img.shape[0]//2, img.shape[1]//2, :])
+    print(img[0:8, 0:8, 1])
+    # print(img[img.shape[0]//2, img.shape[1]//2, :])
     width = img.shape[0]
     height = img.shape[1]
     dct = []
@@ -69,11 +72,22 @@ def cosine_blocking(img):
             for block_x_axis in range(i, i + 8) : 
                 for block_y_axis in range(j, j + 8) : 
                     if (block_x_axis >= width) or (block_y_axis >= height) : 
-                        data = [0, 0, 0]
+                        data = [16, 128, 128]
                     else : 
                         data = img[block_x_axis, block_y_axis, :]
+                        # print(data, "\n\n\n")
                     block[block_x_axis - i][block_y_axis - j] = data
-            dct.append(scipy.fftpack.dct(scipy.fftpack.dct(block, axis = 0, norm = 'ortho'), axis = 1, norm = 'ortho'))
+            if i == 0 and j == 0 :
+                print(np.float32(np.array(block)[:, :, 1]))
+            block = np.array(block)
+            block[:, :, 0] = cv2.dct(np.float32(block[:, :, 0]))
+            block[:, :, 1] = cv2.dct(np.float32(block[:, :, 1]))
+            block[:, :, 2] = cv2.dct(np.float32(block[:, :, 2]))
+            if i == 0 and j == 0 :
+                # print(np.float32(np.array(block)[:, :, 0]))
+                print(np.float32(np.array(block)[:, :, 1]))
+                # print(np.float32(np.array(block)[:, :, 2]))
+            dct.append(block)
     return dct
 
 # function to quantize
@@ -84,8 +98,189 @@ def quantize(img):
         img_quantized[i][:, :, 0] = img[i][:, :, 0] // QY
         img_quantized[i][:, :, 1] = img[i][:, :, 1] // QC
         img_quantized[i][:, :, 2] = img[i][:, :, 2] // QC
-    print(img_quantized[106])
+    # print(img[0], "\n\n\n")
+    # print(img_quantized[0])
     return img_quantized
+
+# function to zigzag
+def zigzag(img):
+    img = quantize(img)
+    # print(img[106])
+    print(img[0][:, :, 0])
+    img_zigzag = img
+    img_zigzagy = []
+    img_zigzagcb = []
+    img_zigzagcr = []
+    for i in range(len(img)):
+        zigzagy = []
+        zigzagcb = []
+        zigzagcr = []
+        zigzagy.append(img[i][0, 0, 0])
+        for j in range(1, 8):
+            if j % 2 == 1:
+                for k in range(j + 1):
+                    zigzagy.append(img[i][k, j - k, 0])
+                    zigzagcb.append(img[i][k, j - k, 1])
+                    zigzagcr.append(img[i][k, j - k, 2])
+            else:
+                for k in range(j, -1, -1):
+                    zigzagy.append(img[i][k, j - k, 0])
+                    zigzagcb.append(img[i][k, j - k, 1])
+                    zigzagcr.append(img[i][k, j - k, 2])
+        for j in range(1, 8):
+            sum = 7 + j
+            if j % 2 == 1:
+                for k in range(7, j - 1, -1):
+                    zigzagy.append(img[i][k, sum - k, 0])
+                    zigzagcb.append(img[i][k, sum - k, 1])
+                    zigzagcr.append(img[i][k, sum - k, 2])
+            else:
+                for k in range(j, 8):
+                    zigzagy.append(img[i][k, sum - k, 0])
+                    zigzagcb.append(img[i][k, sum - k, 1])
+                    zigzagcr.append(img[i][k, sum - k, 2])
+        img_zigzagy.append(zigzagy)
+        img_zigzagcb.append(zigzagcb)
+        img_zigzagcr.append(zigzagcr)
+    return [img_zigzagy, img_zigzagcb, img_zigzagcr]
+
+# function to run-length coding AC
+def run_length_coding(img):
+    img_zigzagy, img_zigzagcb, img_zigzagcr = zigzag(img)
+    rlcAcy = []
+    rlcAcb = []
+    rlcAcr = []
+    rlcDcy = []
+    rlcDcb = []
+    rlcDcr = []
+    zero_count = 0
+    for item in img_zigzagy:
+        rlca = []
+        for value in item[1:]:        
+            if value != 0:            
+                rlca.append([zero_count, value])
+                if zero_count != 0:
+                    zero_count = 0
+            else:
+                zero_count += 1
+        rlca.append([0, 0])
+        rlcAcy.append(rlca)
+    zero_count = 0
+    for item in img_zigzagcb:
+        rlcb = []
+        for value in item[1:]:        
+            if value != 0:            
+                rlcb.append([zero_count, value])
+                if zero_count != 0:
+                    zero_count = 0
+            else:
+                zero_count += 1
+        rlcb.append([0, 0])
+        rlcAcb.append(rlcb)
+    zero_count = 0
+    for item in img_zigzagcr:
+        rlcr = []
+        for value in item[1:]:        
+            if value != 0:            
+                rlcr.append([zero_count, value])
+                if zero_count != 0:
+                    zero_count = 0
+            else:
+                zero_count += 1
+        rlcr.append([0, 0])
+        rlcAcr.append(rlcr)
+    for item in img_zigzagy:
+        rlcDcy.append(item[0])
+        last = item[0]
+        for value in item[1:]:
+            rlcDcy.append(int(value) - int(last))
+            last = value
+    for item in img_zigzagcb:
+        rlcDcb.append(item[0])
+        last = item[0]
+        for value in item[1:]:
+            rlcDcb.append(int(value) - int(last))
+            last = value
+    for item in img_zigzagcr:
+        rlcDcr.append(item[0])
+        last = item[0]
+        for value in item[1:]:
+            rlcDcr.append(int(value) - int(last))
+            last = value
+    return [[rlcAcy, rlcAcb, rlcAcr], [rlcDcy, rlcDcb, rlcDcr]]
+    # print(rlcAc[0])
+    # return rlc
+
+# function to huffman coding
+def huffman_coding(img):
+    rlc = run_length_coding(img)
+    rlcA = rlc[0]
+    rlcA_cof = {}
+    rlcD = rlc[1]
+    rlcD_cof = {}
+    rlcAcy = rlcA[0]
+    rlcAcb = rlcA[1]
+    rlcAcr = rlcA[2]
+    print(rlcAcy[0])
+    rlcDcy = rlcD[0]
+    rlcDcy_cof = {}
+    rlcDcb = rlcD[1]
+    rlcDcb_cof = {}
+    rlcDcr = rlcD[2]
+    rlcDcr_cof = {}
+    for i in range(len(rlcDcy)):
+        rlcDcy_cof[rlcDcy[i]] = len(str(bin(rlcDcy[i])[2:]))
+        rlcDcy[i] = len(str(bin(rlcDcy[i])[2:]))
+    for i in range(len(rlcDcb)):
+        rlcDcb_cof[rlcDcb[i]] = len(str(bin(rlcDcb[i])[2:]))
+        rlcDcb[i] = len(str(bin(rlcDcb[i])[2:]))
+    for i in range(len(rlcDcr)):
+        rlcDcr_cof[rlcDcr[i]] = len(str(bin(rlcDcr[i])[2:]))
+        rlcDcr[i] = len(str(bin(rlcDcr[i])[2:]))
+    rlcD = {}
+    rlcD_cof["DCY"] = rlcDcy_cof
+    rlcD_cof["DCB"] = rlcDcb_cof
+    rlcD_cof["DCR"] = rlcDcr_cof
+    DCfile = open("DCCOFF", "ab")
+    pickle.dump(rlcD_cof, DCfile)
+    DCfile.close()
+    rlcDcyhuff = huffman.HuffmanTree(rlcDcy)
+    rlcDcbhuff = huffman.HuffmanTree(rlcDcb)
+    rlcDcrhuff = huffman.HuffmanTree(rlcDcr)
+    rlcD["DCY"] = rlcDcyhuff.main()
+    rlcD["DCB"] = rlcDcbhuff.main()
+    rlcD["DCR"] = rlcDcrhuff.main()
+    DCfile = open("DC", "ab")
+    pickle.dump(rlcD, DCfile)
+    DCfile.close()
+    ry = {}
+    for i in range(len(rlcAcy)):
+        r = []
+        for j in range(len(rlcAcy[i])):
+            r.append(rlcAcy[i][j][0])
+        rt = huffman.HuffmanTree(r)
+        ry[str(i)] = rt.main()
+    rb = {}
+    for i in range(len(rlcAcb)):
+        r = []
+        for j in range(len(rlcAcb[i])):
+            r.append(rlcAcb[i][j][0])
+        rt = huffman.HuffmanTree(r)
+        rb[str(i)] = rt.main()
+    rr = {}
+    for i in range(len(rlcAcr)):
+        r = []
+        for j in range(len(rlcAcr[i])):
+            r.append(rlcAcr[i][j][0])
+        rt = huffman.HuffmanTree(r)
+        rr[str(i)] = rt.main()
+    rlcA = {}
+    rlcA["ACY"] = ry
+    rlcA["ACB"] = rb
+    rlcA["ACR"] = rr
+    ACfile = open("AC", "ab")
+    pickle.dump(rlcA, ACfile)
+    ACfile.close()
 
 # function open jpeg image
 def open_jpeg(filename):
@@ -107,11 +302,7 @@ if __name__ == '__main__':
     show_image(img, "Original image")
     # plt.show()
 
-    quantize(img)
+    huffman_coding(img)
 
     plt.show()
-
-# function to add x and y
-def add(x, y):
-    return x + y
     
